496cf343563249b9b64d9afbb58160275b075634
michaelangelovalente <miky10.mv@gmail.com>
2022-12-06 15:22:53 +0100

    working on password encryption

---
+++ Example2/springsecurity4/src/main/java/com/example/springsecurity2/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// Connection Tester
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		 
+		String jdbcUrl = "jdbc:mysql://localhost:3306/bank";
+		//String jdbcUrl = "jdbc:mysql://localhost:3306/<insert_schema>?useSSL=false";
+		String user ="root";
+		String pass = "beije";
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			Connection myConn = 
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("Connection successful!");
+		}catch(Exception exc) {
+			exc.printStackTrace();
+		}
+		
+
+	}
+
+}
